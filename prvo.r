ad10cb0356614313a2ba21da2d6fe9b11f5037cf
Gal Drnovšek <gal.drnovsek@gmail.com>
2015-12-09 12:12:13 +0100

    nekaj smo uvozili

--- prvo.r
+++ prvo.r
@@ -7,3 +7,5 @@ url <- "http://czyzewski.org/chess/open-stats.html"
 podatki <- data.frame()
 link <- sprintf(url)
 stran <- html_session(link) %>% read_html(encoding = "UTF-8")
+tabela <- stran %>% html_nodes(xpath ="//table[@height=3334]") %>% html_table()
+data.frame(tabela) -> prva_tabela
