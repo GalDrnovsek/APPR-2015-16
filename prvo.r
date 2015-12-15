5ea694e0355a1f913ef9b0d773353300ae9711e9
GalDrnovsek <gal.drnovsek@gmail.com>
2015-12-15 22:31:12 +0100

    Update prvo.r

--- prvo.r
+++ prvo.r
@@ -9,3 +9,9 @@ link <- sprintf(url)
 stran <- html_session(link) %>% read_html(encoding = "UTF-8")
 tabela <- stran %>% html_nodes(xpath ="//table[@height=3334]") %>% html_table()
 data.frame(tabela) -> prva_tabela
+prva_tabela$X5 <- NULL
+names(prva_tabela)[1] <- "Opening name"
+names(prva_tabela)[2] <- "White win percentage"
+names(prva_tabela)[3] <- "Black win percentage"
+names(prva_tabela)[4] <- "Draw percentage"  
+prva_tabela <- prva_tabela[-1,]
