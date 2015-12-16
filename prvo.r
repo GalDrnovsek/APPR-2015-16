46fa32b4a91a9d72ac52d4bc122ef10cea2919d0
GalDrnovsek <gal.drnovsek@gmail.com>
2015-12-16 22:58:00 +0100

    nekaj novega

--- prvo.r
+++ prvo.r
@@ -2,33 +2,28 @@ require(dplyr)
 require(rvest)
 require(gsubfn)
 
-url <- "http://czyzewski.org/chess/open-stats.html"
-
-link <- sprintf(url)
-stran <- html_session(link) %>% read_html(encoding = "UTF-8")
-tabela <- stran %>% html_nodes(xpath ="//table[@height=3334]") %>% html_table()
-prva_tabela <- data.frame(tabela)
-prva_tabela$X5 <- NULL
-names(prva_tabela)[1] <- "Opening name"
-names(prva_tabela)[2] <- "White win (%)"
-names(prva_tabela)[3] <- "Black win (%)"
-names(prva_tabela)[4] <- "Draw (%)"  
-prva_tabela <- prva_tabela[-1,]
-row.names(prva_tabela) <- 1:137
-
 url1 <- "http://www.becomeawordgameexpert.com/stats.htm"
-
+#Over 2 million chess matches.
 link1 <- sprintf(url1)
 stran1 <- html_session(link1) %>% read_html(encoding = "UTF-8")
 tabela1 <- stran1 %>% html_nodes(xpath = "//table[1]") %>% html_table()
-druga_tabela <- data.frame(tabela1)
-druga_tabela$X5 <- NULL
-names(druga_tabela)[1] <- "Opening name"
-names(druga_tabela)[2] <- "White win (%)"
-names(druga_tabela)[3] <- "Black win (%)"
-names(druga_tabela)[4] <- "Draw (%)"
-druga_tabela <- druga_tabela[-1,]
-row.names(druga_tabela) <- 1:290
+Chess_opening_statistics <- data.frame(tabela1)
+Chess_opening_statistics$X5 <- NULL
+names(Chess_opening_statistics)[1] <- "Opening name"
+names(Chess_opening_statistics)[2] <- "White win (%)"
+names(Chess_opening_statistics)[3] <- "Black win (%)"
+names(Chess_opening_statistics)[4] <- "Draw (%)"
+Chess_opening_statistics <- Chess_opening_statistics[-1,]
+row.names(Chess_opening_statistics) <- 1:290
+
+tabela4 <- stran1 %>% html_nodes(xpath = "//table[2]") %>% html_table()
+Best_white_openings <- data.frame(tabela4)
+
+tabela5 <- stran1 %>% html_nodes(xpath = "//table[4]") %>% html_table()
+Best_black_openings <- data.frame(tabela5)
+
+tabela6 <- stran1 %>% html_nodes(xpath = "//table[6]") %>% html_table()
+Most_drawn_openings <- data.frame(tabela6)
 
 url2 <- "https://en.wikipedia.org/wiki/List_of_chess_grandmasters_by_country"
 
@@ -46,3 +41,9 @@ velemojstri <- velemojstri[-1,]
 row.names(velemojstri) <- 1:65
 velemojstri[,1] <- gsub("(Â\\s)*","",velemojstri[,1])
 velemojstri[,2] <- gsub("(\\[){1}(\\d){1,2}(\\]){1}", "", velemojstri[,2])
+
+link3 <- "https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)"
+stran3 <- html_session(link3) %>% read_html(encoding = "UTF-8")
+tabela3 <- stran3 %>% html_nodes(xpath = "//div[@id='mw-content-text']/table") %>% .[[1]] %>% html_table(fill = TRUE)
+prebivalstvo <- data.frame(tabela3)
+
